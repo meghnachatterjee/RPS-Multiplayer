e3cbc75885bf3c15fd5a1b4ac1165d5ddaa47854
Meghna <meghnanag16@gmail.com>
2018-03-14 21:07:01 -0700

    Initial files

---
+++ assets/config.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDesYURjWqbvRDBEikK3mUwvo7NOtWfDP0",
+    authDomain: "rps-multiplayer-d96bf.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-d96bf.firebaseio.com",
+    projectId: "rps-multiplayer-d96bf",
+    storageBucket: "",
+    messagingSenderId: "397075098309"
+  };
+  firebase.initializeApp(config);
